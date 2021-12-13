9772277a5c24a3ac86329430b4f806da79cc4b59
taystan <t.stanley1412@gmail.com>
2021-12-12 22:31:22 -0500

    New portfolio uploaded

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openHamburgerMenu(){
+    var x = document.getElementById("mainNavBar");
+    if (x.className === "navBar"){
+      x.className += " responsive";
+    } else {
+      x.className = "navBar";
+    }
+  }
