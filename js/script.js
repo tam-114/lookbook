bfe62896e8c61f913eff00e8d11c214a2818835e
taystan <t.stanley1412@gmail.com>
2022-10-12 18:12:13 -0400

    portfolio updates

--- js/script.js
+++ js/script.js
@@ -8,3 +8,37 @@ function openHamburgerMenu(){
     }
   }
 
+
+  function togglePersonalProjects() {
+    var personals = document.getElementsByClassName("notpersonal")
+    Array.from(personals).forEach((personalProjects) => {
+    if (personalProjects.style.display === "none") {
+        personalProjects.style.display = "block";
+    } else {
+        personalProjects.style.display = "none";
+    }
+  })
+}
+
+function toggleFreelanceProjects() {
+    var freelances = document.getElementsByClassName("notfreelance")
+    Array.from(freelances).forEach((freelanceProjects) => {
+    if (freelanceProjects.style.display === "none") {
+        freelanceProjects.style.display = "block";
+    } else {
+        freelanceProjects.style.display = "none";
+    }
+  })
+}
+
+function toggleInternshipProject() {
+    var internship = document.getElementsByClassName("notinternship")
+    Array.from(internship).forEach((internshipProjects) => {
+    if (internshipProjects.style.display === "none") {
+        internshipProjects.style.display = "block";
+    } else {
+        internshipProjects.style.display = "none";
+    }
+  })
+}
+
